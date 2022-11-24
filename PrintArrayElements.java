2afa8086b2ae8f998b4f5f0ed92f739824cc42da
ravi406 <ravikamble406@gmail.com>
2022-11-24 15:16:48 +0530

    Printing array elements

---
+++ PrintArrayElements.java
@@ -0,0 +1,16 @@
+package ArrayPgms;
+
+public class PrintArrayElements {
+
+	public static void main(String[] args) {
+		int array[] = { 10, 20, 30, 40, 50 };
+
+		for (int i = 0; i < array.length; i++) {
+
+			System.out.println(array[i]);
+
+		}
+
+	}
+	
+}
